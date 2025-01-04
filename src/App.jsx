21d4b93f21e32e75be812098316358891fa8dd54
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-01-04 19:23:35 +0600

    Pokemon Card fetching functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Header from './components/Header'
 import SideNav from './components/SideNav'
 import PokeCard from './components/PokeCard'
+import { useState } from 'react'
 
 function App() {
+
+  const [selectedpokemon, setSelectedpokemon] = useState(0);
 
   return (
     <>
       <Header />
-      <SideNav />
-      <PokeCard />
+      <SideNav selectedpokemon={selectedpokemon} setSelectedpokemon={setSelectedpokemon} />
+      <PokeCard selectedpokemon={selectedpokemon} />
     </>
   )
 }
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { getFullPokedexNumber, getPokedexNumber } from "../utils";
 
-const PokeCard = () => {
+const PokeCard = ({ selectedpokemon }) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if(loading || !localStorage) {
+            return;
+        }
+
+        let cache = {};
+        if(localStorage.getItem("pokedex")) {
+            cache = JSON.parse(localStorage.getItem("pokedex"));
+        }
+
+        if(selectedpokemon in cache) {
+            setData(cache[selectedpokemon]);
+            console.log("Data found!"); 
+            return;
+        }
+
+        // fetching pokemon data
+        async function fetchpokemonData() {
+            setLoading(true);
+            try {
+                const baseUrl = "https://pokeapi.co/api/v2/";
+                const suffix = "pokemon/" + getPokedexNumber(selectedpokemon);
+                const finalUrl = baseUrl + suffix;
+                const res = await fetch(finalUrl);
+                const pokemonData = res.json();
+
+                setData(pokemonData);
+                console.log(pokemonData);
+
+                cache[selectedpokemon] = pokemonData;
+                localStorage.setItem("pokedex", JSON.stringify(cache));
+            }
+            catch (error) {
+                console.log(error.message)
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+
+        fetchpokemonData();
+        
+    }, [selectedpokemon]);
+
+    if(loading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+    
     return (
-        <div>
-            
+        <div className="poke-card">
+            <div>
+                <h4>#{getFullPokedexNumber(selectedpokemon)}</h4>
+                <h2></h2>
+            </div>
+
         </div>
     );
 };
